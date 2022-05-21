077aec94e49e827e0a91c2c741551058a6f2424c
Kerim <targetbad@hotmail.com>
2022-05-21 21:35:04 +0300

    Initial Commit

---
+++ SwiftUI WhatsupClone/SwiftUI_WhatsupCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WhatsupCloneApp.swift
+//  SwiftUI WhatsupClone
+//
+//  Created by Kerim Civelek on 21.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WhatsupCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
